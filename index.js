c9e95a485ad8bb5c9d23f0841bf87456b38a6970
Valentin <Vinetos@users.noreply.github.com>
2021-06-15 22:33:59 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,21 +1,24 @@
 const express = require('express')
 const fs = require('fs')
+const { promisify } = require('util');
+const exec = promisify(require('child_process').exec)
 const app = express();
 const port = 3000
 
 
 var concat = require('concat-stream');
-app.use(function(req, res, next){
-  req.pipe(concat(function(data){
+app.use(function(req, res, next) {
+  req.pipe(concat(function(data) {
     req.body = data;
     next();
   }));
 });
 
-app.post('/audio', function (req, res) {
-  console.log(req.body)
-   fs.writeFileSync('oui.wav', Buffer.from(new Uint8Array(req.body))); // write the blob to the server as a file
-  res.sendStatus(200); //send back that everything went ok
+app.post('/audio', async function (req, res) {
+  fs.writeFileSync('oui.wav', Buffer.from(new Uint8Array(req.body))); // write the blob to the server as a file
+  const out = await exec('gcloud ml speech recognize oui.wav --language-code=fr-FR')
+  console.log(out.stdout)
+  res.json(JSON.parse(out.stdout)); //send back that everything went ok
 });
 
 app.listen(port, () => {
