a0b22a9a74e53197ce95cc9516fbc8301a2f8292
Valentin <Vinetos@users.noreply.github.com>
2021-06-16 22:54:14 +0200

    Update index.js

--- index.js
+++ index.js
@@ -16,9 +16,18 @@ app.use(function(req, res, next) {
 
 app.post('/audio', async function (req, res) {
   fs.writeFileSync('oui.wav', Buffer.from(new Uint8Array(req.body))); // write the blob to the server as a file
-  const out = await exec('gcloud ml speech recognize oui.wav --language-code=fr-FR')
-  console.log(out.stdout)
-  res.json(JSON.parse(out.stdout)); //send back that everything went ok
+  const out1 = await exec('gcloud ml speech recognize oui.wav --language-code=fr-FR')
+  const gg = JSON.parse(out1.stdout)
+  const out1 = await exec('./heymorty audio oui.wav')
+  const mrty = JSON.parse(out1.stdout);
+  let res = {results: []};
+  if (abs(gg.confidence - mrty.confidence) < 0.05)
+    res.results.push(gg, mrty);
+  else if (gg.confidence > mrty.confidence)
+    res.results.push(gg)
+  else
+    res.results.push(mrty)
+  res.json(JSON.parse(res)); //send back that everything went ok
 });
 
 app.listen(port, () => {
